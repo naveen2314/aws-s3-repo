c1279ca028b15816b9ddec2915fadf10683fc18f
naveen2314 <naveen2314@yahoo.com>
2024-06-04 20:43:47 +0400

    Create S3 with logging

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ module "s3_bucket" {
   tags       = lookup(each.value, "tags")
   versioning = lookup(each.value, "versioning")
   logging = {
-    bucket        = lookup(each.value, "logging_bucket", "")
-    target_bucket = lookup(each.value, "target_bucket", "")
-    target_prefix = lookup(each.value, "target_prefix", "")
+    logging_bucket = lookup(each.value, "logging_bucket", "")
+    target_bucket  = lookup(each.value, "target_bucket", "")
+    target_prefix  = lookup(each.value, "target_prefix", "")
   }
 
 
